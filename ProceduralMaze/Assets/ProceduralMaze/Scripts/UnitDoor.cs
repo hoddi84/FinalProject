3d5bf33e468d6196938dc4dc8c3bce45c2113d0f
hoddi84 <hordurh15@ru.is>
2017-09-12 00:07:55 +0000

    Doing door mechanics

---
+++ ProceduralMaze/Assets/ProceduralMaze/Scripts/UnitDoor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitDoor : MonoBehaviour {
+
+	public GameObject frame;
+	public GameObject door;
+	private Action onDoorRotChanged = null;
+
+	private void Update() 
+	{
+		if(Input.GetKeyDown(KeyCode.E)) {
+			test();
+			// TODO 
+			// Fix bandwidth recurrency failure.
+		}
+	}
+	private void test() 
+	{
+		print("Frame" + frame.transform.forward);
+		print("Door" + door.transform.forward);
+	}
+
+
+
+
+	private bool IsDoorClosed() 
+	{
+		return false;
+	}
+
+
+}
