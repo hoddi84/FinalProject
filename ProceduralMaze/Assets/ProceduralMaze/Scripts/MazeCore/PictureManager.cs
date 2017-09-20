7bcedc22b49fceb57dda4f0de8d2e3959a31dd54
hoddi84 <hordurh15@ru.is>
2017-09-20 11:15:17 +0000

    Minor aspect changes to pictures

---
+++ ProceduralMaze/Assets/ProceduralMaze/Scripts/MazeCore/PictureManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PictureManager : MonoBehaviour {
+
+	public Sprite[] flowersHealthy;
+	public Sprite[] flowersDying;
+
+	private SpriteRenderer renderer;
+	// Use this for initialization
+	void Start () {
+		
+		renderer = GetComponent<SpriteRenderer>();
+
+		int rndIndex = Random.Range(0, flowersHealthy.Length);
+		renderer.sprite = flowersHealthy[rndIndex];
+	}
+}
