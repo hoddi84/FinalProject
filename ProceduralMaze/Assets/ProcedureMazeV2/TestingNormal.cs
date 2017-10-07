85343efe89b17f31ef15b52fd598629ae869a51a
hoddi84 <hordurh15@ru.is>
2017-10-07 00:57:08 +0000

    Minor changes

---
+++ ProceduralMaze/Assets/ProcedureMazeV2/TestingNormal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingNormal : MonoBehaviour {
+
+	public Vector3 normal;
+	// Use this for initialization
+	void Start () {
+		
+		normal = gameObject.transform.forward;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		normal = gameObject.transform.forward;
+	}
+}
---
+++ ProceduralMaze/Assets/ProcedureMazeV2/TriggerTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTest : MonoBehaviour {
+
+	public GameObject[] connecters;
+
+	private ProcedureMazeManager mazeManager;
+	
+	void Awake()
+	{
+		mazeManager = FindObjectOfType(typeof(ProcedureMazeManager)) as ProcedureMazeManager;
+	}
+
+	void OnTriggerEnter()
+	{
+		mazeManager.InstantiateUnit(connecters);
+	}
+}
