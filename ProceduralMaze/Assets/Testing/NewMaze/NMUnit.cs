31b45ee8aa4500e4851aef11502a78186bfa549d
hoddi84 <hordurh15@ru.is>
2017-10-24 17:52:24 +0000

    Adding UI (In Progress)

---
+++ ProceduralMaze/Assets/Testing/NewMaze/NMUnit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NMUnit : MonoBehaviour {
+
+	public GameObject[] connecters;
+	public GameObject content;
+
+	public void MakeConnecterParent()
+	{
+		content.transform.parent = connecters[0].transform;
+		connecters[1].transform.parent = connecters[0].transform;
+	}
+
+	public void RemoveParent()
+	{
+		content.transform.parent = gameObject.transform;
+		connecters[1].transform.parent = gameObject.transform;
+	}
+}
