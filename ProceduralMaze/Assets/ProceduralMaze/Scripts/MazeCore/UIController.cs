5bf0e3ed4f7775cba6badf2854c56dacb6a3d995
hoddi84 <hordurh15@ru.is>
2017-10-25 14:47:31 +0000

    Minor changes

---
+++ ProceduralMaze/Assets/ProceduralMaze/Scripts/MazeCore/UIController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+	public GameObject simplePanel;
+	public GameObject focusedPanel;
+
+	public Toggle simpleCheck;
+	public Toggle focusedCheck;
+
+	private bool isSimpleCheckOn;
+	private bool isFocusedCheckOn;
+
+	void Start()
+	{
+		isSimpleCheckOn = simpleCheck.isOn;
+		isFocusedCheckOn = focusedCheck.isOn;
+	}
+
+	void SimpleMode()
+	{
+
+	}
+
+	void FocusedMode()
+	{
+		
+	}
+}
