60d0ba6d48bc0feb327b5054c4fa794fe1bdb577
hoddi84 <hordurh15@ru.is>
2017-12-03 22:56:26 +0000

    Minor changes

---
+++ ProceduralMaze/Assets/Testing/ColorPicker/testing.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class testing : MonoBehaviour {
+
+	public GameObject rect;
+
+	private static bool GetLocalMouse( GameObject go, out Vector2 result ) 
+    {
+        var rt = ( RectTransform )go.transform;
+        var mp = rt.InverseTransformPoint( Input.mousePosition );
+
+        result.x = Mathf.Clamp( mp.x, rt.rect.min.x, rt.rect.max.x );
+        result.y = Mathf.Clamp( mp.y, rt.rect.min.y, rt.rect.max.y );
+
+        return rt.rect.Contains( mp );
+    }
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Vector2 vector;
+
+			if (GetLocalMouse(rect, out vector)) {
+				print(vector);
+			}
+		}
+	}
+}
