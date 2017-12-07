a4fd9b35af85cf670ff9dce87596497e92357ad3
Hordur <hordurh15@ru.is>
2017-12-07 12:17:32 +0000

    Minor changes

---
+++ ProceduralMaze/Assets/ProceduralMaze/Scripts/AlignTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlignTest : MonoBehaviour {
+
+	BoxCollider coll;
+	void Start () {
+		
+		coll = GetComponentInChildren<BoxCollider>();
+
+		float xExtents = coll.bounds.extents.x;
+		float yExtents = coll.bounds.extents.z;
+
+		print("X: " + xExtents);
+		print("Z: " + yExtents);
+	}
+}
