7eb166cbb3e2c4f02b252edfb7ffc8e9b9be5c49
Hordur <hordurh15@ru.is>
2017-12-08 20:42:27 +0000

    Adding SoundManager.cs

---
+++ ProceduralMaze/Assets/ProceduralMaze/Scripts/MazeCore/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	public GameObject soundPanel;
+
+	private bool showSoundPanel = false;
+
+	public void ToggleSoundPanel()
+	{
+		showSoundPanel = !showSoundPanel;
+
+		if (showSoundPanel)
+		{
+			soundPanel.SetActive(true);
+		}
+		else
+		{
+			soundPanel.SetActive(false);
+		}
+	}
+}
