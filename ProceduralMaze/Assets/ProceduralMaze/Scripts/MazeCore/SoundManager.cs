aae489bdc0bea608febf2beffe7b7416f0ba85c8
Hordur <hordurh15@ru.is>
2017-12-10 17:57:55 +0000

    Changes to SoundManager

--- ProceduralMaze/Assets/ProceduralMaze/Scripts/MazeCore/SoundManager.cs
+++ ProceduralMaze/Assets/ProceduralMaze/Scripts/MazeCore/SoundManager.cs
@@ -1,12 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public struct AmbienceSound {
+	public Text text;
+	public Toggle activeToggle;
+	public Slider volumeSlider;
+}
 
 public class SoundManager : MonoBehaviour {
 
 	public GameObject soundPanel;
 
+	public AmbienceSoundsScriptableObject ambienceSoundsAsset;
+
 	private bool showSoundPanel = false;
+
+	public AmbienceSound ambienceSound;
+
+	private AudioSource ambienceSource1;
+
+	void Start()
+	{
+		ActivateAmbience();
+		ambienceSound.volumeSlider.onValueChanged.AddListener(OnSliderChanged);
+		ambienceSound.activeToggle.onValueChanged.AddListener(OnToggleChanged);
+	}
+
+	private void OnSliderChanged(float volume)
+	{
+		ambienceSource1.volume = volume;
+	}
+
+	private void OnToggleChanged(bool active)
+	{
+		if (active)
+		{
+			ambienceSource1.Play();
+		}
+		else
+		{
+			ambienceSource1.Stop();
+		}
+	}
 
 	public void ToggleSoundPanel()
 	{
@@ -20,5 +58,22 @@ public class SoundManager : MonoBehaviour {
 		{
 			soundPanel.SetActive(false);
 		}
+	}
+
+	void ActivateAmbience()
+	{
+		GameObject t = GameObject.Find("AmbienceSOunds");
+		if (t == null)
+		{
+			t = new GameObject("AmbienceSounds");
+		}
+
+		ambienceSource1 = t.AddComponent<AudioSource>();
+		ambienceSource1.spatialBlend = 1.0f;
+		ambienceSource1.clip = ambienceSoundsAsset.ambienceSounds[0];
+		ambienceSource1.loop = true;
+		ambienceSource1.playOnAwake = false;
+		ambienceSource1.volume = 0;
+		ambienceSource1.Play();
 	}
 }
---
+++ ProceduralMaze/Assets/ProceduralMaze/Scripts/ScriptableObjects/AmbienceSoundsScriptableObject.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName="AmbienceSoundsAsset", menuName="AmbienceSoundsAsset", order=1)]
+public class AmbienceSoundsScriptableObject : ScriptableObject {
+
+	public AudioClip[] ambienceSounds;
+}
