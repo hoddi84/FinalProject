1d4e6fd2db02e48875ff9f032489d1e8c3f4132c
hoddi84 <hordurh15@ru.is>
2018-01-09 17:13:11 +0000

    Minor changes

---
+++ ProceduralMaze/Assets/ProceduralMaze/Scripts/MazeCore/DecalManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecalManager : MonoBehaviour {
+
+	private SimpleInterfaceController simpleInterface;
+
+	[Range(0.0f, 1.0f)]
+	public float bloodDecal;
+	private float _bloodDecal;
+
+	public Action<float> onBloodDecalChanged = null;
+
+	void Awake()
+	{
+		simpleInterface = FindObjectOfType(typeof(SimpleInterfaceController)) as SimpleInterfaceController;
+
+		if (simpleInterface != null)
+		{
+			bloodDecal = simpleInterface.scarySlider.value;
+			_bloodDecal = bloodDecal;
+			
+			simpleInterface.onScarySliderChanged += UpdateBloodDecals;
+		}
+		else
+		{
+			bloodDecal = 0;
+			_bloodDecal = bloodDecal;
+		}
+	}
+
+	void Update()
+	{
+		if (_bloodDecal != bloodDecal)
+		{
+			UpdateBloodDecals(bloodDecal);
+		}
+	}
+
+	void UpdateBloodDecals(float newValue)
+	{
+		bloodDecal = newValue;
+		_bloodDecal = bloodDecal;
+
+		if (onBloodDecalChanged != null)
+		{
+			onBloodDecalChanged(bloodDecal);
+		}
+	}
+}
