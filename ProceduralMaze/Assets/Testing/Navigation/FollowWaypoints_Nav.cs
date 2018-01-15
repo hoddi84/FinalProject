2a2f7747470bc30fef34d5f5e549dcaa2c2ae6af
hoddi84 <hordurh15@ru.is>
2018-01-15 20:55:27 +0000

    Navigation

---
+++ ProceduralMaze/Assets/Testing/Navigation/FollowWaypoints_Nav.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowWaypoints_Nav : MonoBehaviour {
+
+	public Transform[] waypoints;
+	public float speed = .5f;
+	public float accuracy = .1f;
+	private int currentWaypoint = 0;
+
+	private void LateUpdate()
+	{
+		Vector3 goalPosition = waypoints[currentWaypoint].position - transform.position;
+
+		if (goalPosition.magnitude < accuracy)
+		{
+			currentWaypoint++;
+			if (currentWaypoint >= waypoints.Length)
+			{
+				currentWaypoint = 0;
+			}
+		}
+		transform.position += goalPosition.normalized * speed * Time.deltaTime;
+	}
+}
---
+++ ProceduralMaze/Assets/Testing/Navigation/movetest1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movetest1 : MonoBehaviour {
+
+	public Transform goal;
+	public float speed = 0.5f;
+	public bool useTranslate = false;
+
+	void Start()
+	{
+		//transform.LookAt(goal.position);
+	}
+
+	private void LateUpdate()
+	{
+		WithTranslate(useTranslate);
+
+		Debug.DrawRay(transform.position, transform.forward, Color.blue);
+	}
+
+	private void WithTranslate(bool useTranslate)
+	{
+		Vector3 goalposition = goal.transform.position - transform.position;
+		Debug.DrawLine(transform.position, goal.position, Color.red);
+		transform.LookAt(goal.position);
+
+		if (useTranslate)
+		{
+			transform.Translate(goalposition.normalized * speed * Time.deltaTime, Space.World);
+		}
+		else
+		{
+
+			transform.position += goalposition * speed * Time.deltaTime;
+		}
+	}
+}
