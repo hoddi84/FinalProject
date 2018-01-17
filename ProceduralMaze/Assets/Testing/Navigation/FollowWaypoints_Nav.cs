c3e59b99249de5af895e23761106f0f0421b57c4
Hordur <hordurh15@ru.is>
2018-01-17 00:02:41 +0000

    Minor changes

--- ProceduralMaze/Assets/Testing/Navigation/FollowWaypoints_Nav.cs
+++ ProceduralMaze/Assets/Testing/Navigation/FollowWaypoints_Nav.cs
@@ -6,14 +6,22 @@ public class FollowWaypoints_Nav : MonoBehaviour {
 
 	public Transform[] waypoints;
 	public float speed = .5f;
+	public float rotSpeed = .1f;
 	public float accuracy = .1f;
 	private int currentWaypoint = 0;
 
 	private void LateUpdate()
 	{
-		Vector3 goalPosition = waypoints[currentWaypoint].position - transform.position;
+		Debug.DrawRay(transform.position, transform.forward*5, Color.blue);
 
-		if (goalPosition.magnitude < accuracy)
+		//Vector3 goalPosition = waypoints[currentWaypoint].position - transform.position;
+		Vector3 goalPosition = new Vector3(waypoints[currentWaypoint].position.x, transform.position.y, waypoints[currentWaypoint].position.z);
+
+		Vector3 dir = goalPosition - transform.position;
+
+		this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * rotSpeed);
+
+		if (dir.magnitude < accuracy)
 		{
 			currentWaypoint++;
 			if (currentWaypoint >= waypoints.Length)
@@ -21,6 +29,7 @@ public class FollowWaypoints_Nav : MonoBehaviour {
 				currentWaypoint = 0;
 			}
 		}
-		transform.position += goalPosition.normalized * speed * Time.deltaTime;
+		//transform.position += dir.normalized * speed * Time.deltaTime;
+		transform.Translate(0, 0, speed * Time.deltaTime);
 	}
 }
