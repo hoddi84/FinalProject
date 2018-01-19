90d2535ec6adc559db7d2c642c2d7db0725f64c8
Hordur <hordurh15@ru.is>
2018-01-19 21:29:28 +0000

    Minor changes

---
+++ ProceduralMaze/Assets/Testing/QueueTesting/Queue_VisualScan.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Queue_VisualScan : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.DrawRay(transform.position, transform.forward * 5, Color.red);
+		
+	}
+}
