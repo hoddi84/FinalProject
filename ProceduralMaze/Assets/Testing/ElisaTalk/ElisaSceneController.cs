4f9b5a8eff50d48de0be7868e67bea0ac18668e3
hoddi84 <hordurh15@ru.is>
2018-01-26 01:39:47 +0000

    Added test scene for Elisa

---
+++ ProceduralMaze/Assets/Testing/ElisaTalk/ElisaSceneController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElisaSceneController : MonoBehaviour {
+
+	private Animator animator;
+	
+	private bool isWaving = false;
+	private bool isDancing = false;
+
+	void Awake()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			if (isWaving)
+			{
+				animator.SetBool("Wave", false);
+				isWaving = false;
+			}
+			else
+			{
+				animator.SetBool("Wave",true);
+				isWaving = true;
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			if (isDancing)
+			{
+				animator.SetBool("Dance", false);
+				isDancing = false;
+			}
+			else
+			{
+				animator.SetBool("Dance", true);
+				isDancing = true;
+			}
+		}
+	}
+}
