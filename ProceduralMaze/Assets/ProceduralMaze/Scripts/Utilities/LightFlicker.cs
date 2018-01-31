f5209014ff243ccd77472758cb04e78089bdd427
Hordur <hordurh15@ru.is>
2018-01-31 15:01:48 +0000

    Minor changes

---
+++ ProceduralMaze/Assets/ProceduralMaze/Scripts/Utilities/LightFlicker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+	private Color currentLightColor;
+	private Color currentAmbienceColor;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+
+		}
+	}
+
+	IEnumerator FlickerLights()
+	{
+		yield return null;
+	}
+
+	void TurnOffAllLights(Light[] lights, float duration)
+	{
+		foreach (Light light in lights)
+		{
+			
+		}
+	}
+
+	void TurnOnAllLights(Light[] lights, float duration)
+	{
+
+	}
+}
