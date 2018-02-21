473879fa44ce098502e27c3022127c9fd52a8f77
Hordur <hordurh15@ru.is>
2018-02-21 14:09:08 +0000

    Minor changes

---
+++ ProceduralMaze/Assets/Testing/GPUBuilder/TestToggle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestToggle : MonoBehaviour {
+
+	private GPUSort sort;
+	public GameObject obj;
+	private bool showInstanced = true;
+
+	void Awake()
+	{
+		sort = FindObjectOfType<GPUSort>();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			showInstanced = !showInstanced;
+
+			if (showInstanced)
+			{
+				sort.enabled = true;
+				obj.SetActive(false);
+			}
+			else
+			{
+				sort.enabled = false;
+				obj.SetActive(true);
+			}
+		}
+	}
+}
