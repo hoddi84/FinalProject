c0b95c4060d42465b83c7769a4839b6c6accf983
hoddi84 <hordurh15@ru.is>
2018-03-03 02:54:23 +0000

    Minor changes

---
+++ ProceduralMaze/Assets/ProceduralMaze/Scripts/MazeCore/UnitPathDrawer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitPathDrawer : MonoBehaviour {
+
+	private Collider meshCollider;
+	private LineRenderer line;
+	private Vector3 leftPosition;
+	private Vector3 rightPosition;
+
+	private float extentsX;
+
+	private void Start()
+	{
+		line = gameObject.GetComponent<LineRenderer>();
+		meshCollider = GetComponent<MeshCollider>();
+
+		extentsX = meshCollider.bounds.extents.x;
+
+		leftPosition = new Vector3(transform.position.x + extentsX, transform.position.y, transform.position.z);
+		rightPosition = new Vector3(transform.position.x - extentsX, transform.position.y, transform.position.z);
+
+		leftPosition += transform.forward;
+		rightPosition += transform.forward;
+	}
+
+	private void Update()
+	{
+
+		line.SetPositions(
+			new Vector3[] {
+				leftPosition, 
+				rightPosition
+			}
+		);
+	}
+}
